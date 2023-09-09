3501e127a47fba881b3a99f76ee78fcc499a6dd8
belapko <belapko23@gmail.com>
2023-09-09 14:25:28 +0300

    added routes

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,9 +1,10 @@
-import { BuildOptions } from "./types/config";
-import {Configuration as DevServerConfiguration} from 'webpack-dev-server';
+import { BuildOptions } from './types/config';
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
-export function buildDevServer (options: BuildOptions): DevServerConfiguration {
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port, // server port
         open: true, // open new window in browser when server starts
+        historyApiFallback: true, // Allows to proxy requests through a specified index page (by default 'index.html'), useful for Single Page Applications that utilise the HTML5 History API
     };
-};
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './index.scss';
 import { Counter } from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPage } from './pages/AboutPage/AboutPage';
+import { MainPage } from './pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
 
 export const App = () => {
     return (
         <div className='app'>
-            App
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage />} />
+                <Route path={'/'} element={<MainPage />} />
+            </Routes>
         </div>
     );
 };
