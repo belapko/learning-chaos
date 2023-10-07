3bd0de7a760df8926ea871b94fb9c90068449915
belapko <belapko23@gmail.com>
2023-10-08 01:47:37 +0300

    fixed translation

--- src/features/auth-username/model/services/login-username/loginByUsername.ts
+++ src/features/auth-username/model/services/login-username/loginByUsername.ts
@@ -1,5 +1,4 @@
 import { User, userActions } from '@/entities/user';
-import i18n from '@/shared/config/i18n/i18n';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from '../../constants';
@@ -26,7 +25,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
 			return response.data;
 		} catch (e) {
 			console.log(e);
-			return thunkAPI.rejectWithValue(i18n.t('Oops! Either this is not your account, or you entered the wrong password.'));
+			return thunkAPI.rejectWithValue('ERROR');
 		}
 	}
 );
--- src/features/auth-username/ui/login/LoginForm.tsx
+++ src/features/auth-username/ui/login/LoginForm.tsx
@@ -43,7 +43,7 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
 
 	return (
 		<div className={classNames(styles.form, [className])}>
-			{error && <Text className={styles.width} paragraph={error} isError={true} />}
+			{error && <Text className={styles.width} paragraph={t('Oops! Either this is not your account, or you entered the wrong password.')} isError={true} />}
 			<Input className={styles.width} type='text' label='Имя пользователя' autofocus={true} value={username} onChange={onChangeUsername} />
 			<Input className={styles.width} type='password' label='Пароль' value={password} onChange={onChangePassword} />
 			<Button className={styles.width} theme={ButtonTheme.OUTLINE} onClick={onLoginClick} disabled={isLoading}>
